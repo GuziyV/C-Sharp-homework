32982de8c2b9c303fbcca91dae17ce0c83d1c038
GuziyV <volodyaguziy3@gmail.com>
2018-05-08 15:53:47 +0300

    Add class Car and enum CarType
    Car with fields Id, CarType and Balance

---
+++ Homework/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Car
+    {
+        public Car(uint id, CarType type, int balance = 0)
+        {
+            Id = id;
+            Type = type;
+            Balance = balance;
+
+        }
+        public uint Id { get; private set; }
+
+        public readonly CarType Type;
+
+        public int Balance { get; private set; }
+    }
+
+    enum CarType
+    {
+        Passenger,
+        Truck,
+        Bus,
+        Motorcycle
+    }
+}
